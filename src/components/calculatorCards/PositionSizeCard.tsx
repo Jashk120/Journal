648947a4558c4e81492e012a59b55c4e42eafeeb
Ainz <127408603+Jashk120@users.noreply.github.com>
2024-11-20 12:37:55 +0530

    Update PositionSizeCard.tsx

--- src/components/calculatorCards/PositionSizeCard.tsx
+++ src/components/calculatorCards/PositionSizeCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CardContent, CardFooter, CardHeader } from '../ui/card';
-import { Input } from '../ui/Input';
+import { Input } from '../ui/input';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card';
 import { FaInfoCircle } from 'react-icons/fa';
 import { Separator } from '../ui/separator';
